83deea3618f28d7cb403c668b5dca06a7818b02c
carter <102479896+Carter907@users.noreply.github.com>
2023-05-06 13:06:24 -0400

    added callback parameter

--- src/main/kotlin/carte/toolfx/core/Controller.kt
+++ src/main/kotlin/carte/toolfx/core/Controller.kt
@@ -3,6 +3,7 @@ package carte.toolfx.core
 import javafx.fxml.Initializable
 import javafx.scene.Node
 import javafx.scene.Scene
+import javafx.util.Callback
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -11,12 +12,15 @@ abstract class Controller: Initializable {
 
     protected lateinit var scene: Scene;
 
-    fun setLater(node: Node) {
+    fun setLater(node: Node, after: () -> Unit) {
 
         GlobalScope.launch {
             while (node.scene == null) { delay(100) }
 
             scene = node.scene;
+
+            after.invoke();
+
         }
 
     }
